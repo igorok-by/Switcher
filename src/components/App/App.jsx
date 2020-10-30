6f2093e3166b519b8743a3e8c87aa9c7f3413fab
Igor <igorko.by@gmail.com>
2020-10-30 13:45:06 +0300

    feat: add rendering name depends on input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.scss'
 
+import FRUITS from '../../data/fruits'
+
 const App = () => {
+  const [fruitId, setFruitId] = useState(1)
+
+  const fruitName = FRUITS.find((fruit) => fruit.id === +fruitId).name
+
+  const handleValueChange = (event) => {
+    setFruitId(event.target.value)
+  }
+
   return (
     <form className="app">
-      <p className="app__title">Apple</p>
-      <input type="text" className="app__input" value={1} />
+      <p className="app__title">{fruitName}</p>
+      <input
+        type="text"
+        className="app__input"
+        value={fruitId}
+        onChange={handleValueChange}
+      />
     </form>
   )
 }
