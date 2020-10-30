1955f2fa1b4f32297d7b6469e865cc7ba2370e91
Igor <igorko.by@gmail.com>
2020-10-30 17:09:14 +0300

    build: add /build to origin

---
+++ build/precache-manifest.8701984351a2691dc38b16dfdbf33900.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be3d93b636e8361a881faf9f0e2b823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef890975b66a9565b79a",
+    "url": "/static/css/main.7f60882f.chunk.css"
+  },
+  {
+    "revision": "4bb51ce7ec5f35a4507d",
+    "url": "/static/js/2.35c216fb.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.35c216fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef890975b66a9565b79a",
+    "url": "/static/js/main.644c91c0.chunk.js"
+  },
+  {
+    "revision": "13f6d0472e00a805e14f",
+    "url": "/static/js/runtime-main.b1883ed1.js"
+  },
+  {
+    "revision": "4312f1fbdcf4d54af4506dabdce08010",
+    "url": "/static/media/Roboto-Regular.4312f1fb.ttf"
+  },
+  {
+    "revision": "4dd091c2e8dae39d9cfe10e4b5967ad4",
+    "url": "/static/media/Roboto-Regular.4dd091c2.woff"
+  },
+  {
+    "revision": "b9077621ce786b55c176a61456bfc077",
+    "url": "/static/media/Roboto-Regular.b9077621.eot"
+  },
+  {
+    "revision": "d0c5cfd9c2b4fb42753f67ab1418f225",
+    "url": "/static/media/Roboto-Regular.d0c5cfd9.svg"
+  }
+]);
